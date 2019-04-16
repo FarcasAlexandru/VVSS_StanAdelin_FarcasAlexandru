1f0b20cf723430e3968f55a815445f5fcc53c9b9
Farcas_Alexandru <farcas.alexandru0@gmail.com>
2019-04-16 20:36:48 +0300

    Lab03_TakeHome

--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -29,31 +29,72 @@ public class ServiceTest {
     @Test
     public void saveStudent1() {
         int result = service.saveStudent("1", "Bob", 500);
-        assert( result == 0 );
+        assert (result == 0);
     }
 
     @Test
     public void saveStudent2() {
         int result = service.saveStudent("1", "Bob", -1);
-        assert( result == 1 );
+        assert (result == 1);
     }
 
     @Test
     public void saveStudent3() {
         int result = service.saveStudent("", "Bob", 500);
-        assert( result == 1 );
+        assert (result == 1);
     }
 
     @Test
     public void saveStudent4() {
         int result = service.saveStudent("1", "", 500);
-        assert( result == 1 );
+        assert (result == 1);
     }
 
     @Test
     public void saveStudent5() {
         int result = service.saveStudent("1", "Bob", 500);
-        assert( result == 0 );
+        assert (result == 0);
     }
 
+    @Test
+    public void addAssignment_InvalidID() {
+        int result = service.saveTema("", "descriere", 1, 1);
+        assert( result == 1 );
+    }
+
+    @Test
+    public void addAssignment_InvalidDescription() {
+        int result = service.saveTema("id", "", 1, 1);
+        assert( result == 1 );
+    }
+
+    @Test
+    public void addAssignment_InvalidLowerBoundDeadline() {
+        int result = service.saveTema("id", "descriere", -1, 1);
+        assert( result == 1 );
+    }
+
+    @Test
+    public void addAssignment_InvalidUpperBoundDeadline(){
+        int result = service.saveTema("id","descriere",15,1);
+        assert( result == 1 );
+    }
+
+    @Test
+    public void addAssignment_InvalidDealineLowerThanStartline(){
+        int result = service.saveTema("id", "descriere",4,5);
+        assert( result == 1);
+    }
+
+    @Test
+    public void addAssignment_InvalidLowerBoundStartline(){
+        int result = service.saveTema("id", "descriere", 1,-1);
+        assert( result == 1);
+    }
+
+    @Test
+    public void addAssignment_InvalidUpperBoundStartline(){
+        int result = service.saveTema("id","descriere",15,15);
+        assert( result == 1);
+    }
 }
